ab8a080da97b1aea31eea566a049f662e5f9ba18
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-02-16 05:57:15 -0500

    complete the posts part

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,22 +1,13 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import WhoToFollowList from "../WhoToFollowList/index.js";
+import PostSummaryList from "../PostSummaryList/index.js";
 function exploreComponent() {
     $('#wd-explore').append(`
         <div class="row">
             <div class="col-xl-2 col-lg-1 col-md-2 col-sm-2 col-1">${NavigationSidebar()}</div>
-            <div class="col-xl-6 col-lg-7 col-md-9 col-sm-10 col-11 bg-success">.col-6 .col-md-4</div>
+            <div class="col-xl-6 col-lg-7 col-md-9 col-sm-10 col-11">${PostSummaryList()}</div>
             <div class="col-4 d-none d-lg-block">${WhoToFollowList()}</div>
         </div>
     `);
-
-//     $('#wd-explore').append(`
-//         <div class="row wd-layout">
-//             <div class="col-xl-2 col-lg-1 col-md-2 col-sm-2 col-1">${NavigationSidebar()}</div>
-//         </div>
-// `);
-//     $('.wd-layout').append(`
-//         <div class="col-xl-6 col-lg-7 col-md-9 col-sm-10 col-11">${NavigationSidebar()}</div>
-//         <div class="col-4 d-none d-lg-block">${WhoToFollowList()}</div>
-//     `);
 }
 $(exploreComponent);
