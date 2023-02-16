a70eee2ae32cc49d81d04883f1bdc16bdf6aa6f5
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-02-15 19:09:51 -0500

    add the right bar

---
+++ public/tuiter/WhoToFollowList/Who.js
@@ -0,0 +1,13 @@
+export default [
+    {   avatarIcon: '../../images/Java.jpeg',
+        userName: 'Java', handle: '@Java', },
+    {   avatarIcon: '../../images/relativity.jpeg',
+        userName: 'Relativity Space',
+        handle: '@relativityspace', },
+    {   avatarIcon: '../../images/vigin.svg',
+        userName: 'Virgin Galactic',
+        handle: '@virgingalactic', },
+    {   avatarIcon: '../../images/nasa.jpeg',
+        userName: 'NASA', handle: '@NASA', },
+    {   avatarIcon: '../../images/TESLA.PNG',
+        userName: 'Tesla', handle: '@Tesla', }, ];
--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,21 +1,24 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 function exploreComponent() {
     $('#wd-explore').append(`
 <!--       <h2>Explore</h2>-->
   <div class="row">
-    <div class="col-xl-2 col-lg-1 col-md-2 col-sm-2 col-1 bg-warning">
+    <div class="col-xl-2 col-lg-1 col-md-2 col-sm-2 col-1">
 <!--    <h3>NavigationSidebar</h3>-->
         ${NavigationSidebar()}
    </div>
-    <div class="col-xl-6 col-lg-7 col-md-9 col-sm-10 col-11 bg-primary">
+    <div class="col-xl-6 col-lg-7 col-md-9 col-sm-10 col-11">
 <!--    <h3>ExploreComponent</h3>-->
-${NavigationSidebar()}
    </div>
-    <div class="col-4 d-none d-lg-block bg-danger">
+    <div class="col-4 d-none d-lg-block">
 <!--    <h3>WhoToFollowList </h3>-->
-${NavigationSidebar()}
+        ${WhoToFollowList()}
    </div>
   </div>
+  <br>
+  <br>
+  <br>
    `);
 }
 $(exploreComponent);
