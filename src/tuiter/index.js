ef4274037215b8c917d4c798467b6817830f686f
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-02-20 09:52:37 -0500

    complete the who-to-follow part

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -2,14 +2,33 @@ import {Link} from "react-router-dom";
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowListItem from "./who-to-follow/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow";
+import PostSummaryItem from "./post-summary-list/post-summary-item";
+import PostSummaryList from "./post-summary-list";
 
 function Tuiter() {
     return(
         <div>
             <Nav/>
             <NavigationSidebar active="home"/>
-            <WhoToFollowListItem/>
-            <h1>Tuiter</h1>
+            <WhoToFollowList/>
+            <PostSummaryList/>
+            {/*<PostSummaryItem/>*/}
+            {/*<WhoToFollowListItem who={{*/}
+            {/*    userName: 'NASA', handle: 'NASA',*/}
+            {/*    avatarIcon: 'nasa.jpeg',*/}
+            {/*}}/>*/}
+            {/*<WhoToFollowListItem who={{*/}
+            {/*    userName: 'Tesla', handle: 'tesla',*/}
+            {/*    avatarIcon: 'TESLA.PNG',*/}
+            {/*}}/>*/}
+            {/*<WhoToFollowListItem who={{*/}
+            {/*    userName: 'Relativity', handle: 'Relativity',*/}
+            {/*    avatarIcon: 'relativity.jpeg',*/}
+            {/*}}/>*/}
+
+            {/*<WhoToFollowListItem/>*/}
+            {/*<h1>Tuiter</h1>*/}
         </div>
     );
 };
--- src/tuiter/who-to-follow/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow/who-to-follow-list-item.js
@@ -1,21 +1,21 @@
 import React from "react";
 const WhoToFollowListItem = (
     {
-        who = {userName: 'NASA', handle: 'NASA', avatarIcon:'NASA.webp'}
+        who = {userName: 'NASA', handle: 'NASA', avatarIcon:'nasa.jpeg'}
     }
 ) => {
     return(
         <li className="list-group-item">
-            <div className="row">
-                <div className="col-2">
-                    <img className="rounded-circle" height={48} src={`/images/${who.avatarIcon}`}/>
+            <div className="row mb-2">
+                <div className="col-2 d-flex align-items-center">
+                    <img className="rounded-circle" height={80} src={`../../images/${who.avatarIcon}`}/>
                 </div>
-                <div className="col-8">
+                <div className="col-8 align-items-center">
                     <div className="fw-bold">{who.userName}</div>
                     <div>@{who.handle}</div>
                 </div>
-                <div className="col-2">
-                    <button className="btn btn-primary rounded-pill float-end">Follow</button>
+                <div className="col-1 d-flex align-items-center">
+                    <button className="btn btn-primary btn-lg rounded-pill float-end">Follow</button>
                 </div>
             </div>
         </li>
