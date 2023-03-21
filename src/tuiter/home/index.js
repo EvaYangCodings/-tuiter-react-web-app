65fea8530923e22a5a5f3784e3ba997a9b832a0f
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-03-20 22:17:34 -0400

    create a form to allow posting new tuit

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./index.css";
 import TuitsList from "../tuits/tuits-list";
+import WhatsHappening from "./whats-happening";
 const HomeComponent = () => {
     return (
-        <TuitsList/>
+        <>
+            <h4>Home</h4>
+            <WhatsHappening/>
+            <TuitsList/>
+        </>
     )
 }
 
