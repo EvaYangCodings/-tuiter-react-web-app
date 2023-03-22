d454a1d770952d48f1f54575eca7158cda605947
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-03-22 10:02:16 -0400

    finish the profile page

--- src/tuiter/profile/index.js
+++ src/tuiter/profile/index.js
@@ -3,12 +3,31 @@ import {Link} from "react-router-dom";
 import React from "react";
 import "./index.css"
 const ProfileComponent = () => {
+    const formatDate = (dateString) => {
+        if (dateString.includes('/')) {
+            const parts = dateString.split('/');
+            if (parts.length === 3) {
+                // Format: "7/7/1968"
+                const [month, day, year] = parts;
+                const date = new Date(year, month - 1, day);
+                return date.toLocaleString('en-US', { month: 'long', year: 'numeric' });
+            } else if (parts.length === 2) {
+                // Format: "4/2009"
+                const [month, year] = parts;
+                const date = new Date(year, month - 1);
+                return date.toLocaleString('en-US', { month: 'long', year: 'numeric' });
+            }
+        }
+        return dateString; // Fallback to return the input string if not formatted as expected
+    };
+
+
     const user = useSelector(state => state.profile);
     return(
         <>
 
             <div className="row">
-                <div className="col-2">
+                <div className="col-1 ms-3">
                     <Link to="/tuiter/home">
                         <i className="bi bi-arrow-left"/>
                     </Link>
@@ -23,7 +42,7 @@ const ProfileComponent = () => {
             {/*</div>*/}
             <div className="position-relative mb-2">
                 <img alt="" src="../../images/banner-web-development.png" className="w-100"/>
-                <img className="position-absolute w-25 rounded-circle wd-profile-photo"
+                <img alt="" className="position-absolute w-25 rounded-circle wd-profile-photo"
                      src="../../images/react.jpeg"
                 />
                 <button className="float-end mt-3 rounded-pill btn btn-light"><b>Edit profile</b></button>
@@ -32,12 +51,20 @@ const ProfileComponent = () => {
             <div className="wd-user-name"><h4>{user.firstName}{' '}{user.lastName}</h4></div>
             <div>{user.handle}</div>
             <div className="mt-3">{user.bio}</div>
-            <div className="text-primary fs-2">
-                <i className="bi bi-card-image me-3"/>
-                <i className="bi bi-filetype-gif me-3"/>
-                <i className="bi bi-bar-chart me-3"/>
-                <i className="bi bi-emoji-smile me-3"/>
-                <i className="bi bi-geo-alt"/>
+            <div className="row text-secondary mt-1">
+                <div className="col-3 pe-0 me-0">
+                    <i className="bi bi-geo-alt me-1"/>{user.location}
+                </div>
+                <div className="col-2 ps-0 ms-0">
+                    <i className="bi bi-balloon me-1"/>{formatDate(user.dateOfBirth)}
+                </div>
+                <div className="col-3 ps-0 ms-0">
+                    <i className="bi bi-calendar-check me-1"/>Joined{' '}{formatDate(user.dateJoined)}
+                </div>
+            </div>
+            <div className="row mt-2">
+                <div className="col-3"><span className="fw-bold">{user.followingCount}</span>{' '}Following</div>
+                <div className="col-3 ms-0 ps-0"><span className="fw-bold">{user.followersCount}</span>{' '}Followers</div>
             </div>
         </>
 
